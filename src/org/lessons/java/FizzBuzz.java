6ad11ebd1fa44264f386491b8720e25aee3085fb
Ali-Chouchene <choucheneali9801@gmail.com>
2023-05-04 17:27:15 +0200

    fizzbuzz done

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -9,20 +9,39 @@ public class FizzBuzz {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Quanti numeri vuoi estrarre?");
 		String inStr = input.nextLine();
-		int inNumber =Integer.valueOf(inStr);
-		System.out.println("-----------------------------------------------");
-		for (int i = 1; i <= inNumber; i++) {
-			 if (i % (5 * 3) == 0) {
-				 System.out.println("FizzBuzz");
-			 }else if((i % 3) == 0) {
-				 System.out.println("Fizz");
-			 }else if((i % 5) == 0) {
-				 System.out.println("Buzz");
-			 }else {
-				 System.out.println(i);
-			 }
+		int inNumber = 0;
+		if(inStr != "") {
+			inNumber = Integer.valueOf(inStr);
 		}
+		System.out.println("-----------------------------------------------");
 		input.close();
-	}
+		if(inNumber > 0) {
+			for (int i = 1; i <= inNumber; i++) {
+				 if (i % (5 * 3) == 0) {
+					 System.out.println("FizzBuzz");
+				 }else if((i % 3) == 0) {
+					 System.out.println("Fizz");
+				 }else if((i % 5) == 0) {
+					 System.out.println("Buzz");
+				 }else {
+					 System.out.println(i);
+				 }
+			}
+		}else {
+			for (int i = 1; i <= 100; i++) {
+				 if (i % (5 * 3) == 0) {
+					 System.out.println("FizzBuzz");
+				 }else if((i % 3) == 0) {
+					 System.out.println("Fizz");
+				 }else if((i % 5) == 0) {
+					 System.out.println("Buzz");
+				 }else {
+					 System.out.println(i);
+				 }
+			}
+		}
+		}
+
+	
 
 }
